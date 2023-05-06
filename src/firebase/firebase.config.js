5d8ce77873993a7c99e071c2da1fd1f9ba5234e4
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-05-06 13:01:01 +0600

    added login and submit style

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMkvpu4VGCsCtytsG-biGyDmU4KxnbDYI",
+  authDomain: "ema-jhon-with-auth-950d0.firebaseapp.com",
+  projectId: "ema-jhon-with-auth-950d0",
+  storageBucket: "ema-jhon-with-auth-950d0.appspot.com",
+  messagingSenderId: "144999080586",
+  appId: "1:144999080586:web:aedae04cf59fe1ddb7c373"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
