899239b2e02cfe210a29decd5b581ba134956d23
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-05-06 17:43:14 +0600

    finish the project with auth -firebase

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,28 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './Login.css';
-import { Link} from 'react-router-dom';
+import { Link,useNavigate,useLocation} from 'react-router-dom';
 import { AuthContext } from '../Providers/AuthProvider';
 
 const Login = () => {
-    const {signIn} = useContext(AuthContext);
+    const [show,setShow] = useState(false);
 
-    const handleLogin = event =>{
+    const {signIn} = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || '/';
+    
+
+    function handleLogin(event) {
         event.preventDefault();
 
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email,password);
+        console.log(email, password);
 
-        signIn(email,password)
-        .then(result =>{
-            const loggedUser = result.user;
-            console.log(loggedUser);
-            form.reset();
-        })
-        .catch(error =>{
-            console.log(error);
-        })
+        signIn(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                form.reset();
+                navigate(from,{replace:true})
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     return (
@@ -35,7 +43,12 @@ const Login = () => {
                 </div>
                 <div className='form-control'>
                     <label htmlFor="password">Password</label>
-                    <input type="password" name='password' required />
+                    <input type={show? 'text' : 'password'} name='password' required />
+                    <p onClick={() =>setShow(!show)}><small>
+                        {
+                            show ? <span>Hide Password</span> : <span>Show Password</span>
+                        }
+                        </small></p>
                 </div>
                 <input className='btn-submit' type="submit" value='Login' />
             </form>
